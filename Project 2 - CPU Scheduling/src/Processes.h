6445d867275bcba42e672fa9729111c465d6c460
LiamRobertson327 <liam327@verizon.net>
2025-10-03 15:16:12 -0700

    Added comments to code.

--- Project 2 - CPU Scheduling/src/Processes.h
+++ Project 2 - CPU Scheduling/src/Processes.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 
+//Data structure for each process
 struct Process {
 
     float arrivalTime;
@@ -11,10 +12,14 @@ struct Process {
     char id;
 };
 
+//Function to create a new process and initialize with random values
 Process createProcess(){
     Process process;
 
+    //Random number generator to produce non-deterministic random numbers
     std::random_device rd;
+
+    //Mersenne Twister pseudo-random generator of 32-bit numbers
     std::mt19937 gen(rd());
 
     //Float from 0 inclusive to 99 exclusive
@@ -26,6 +31,7 @@ Process createProcess(){
     //Int from 1 to 4 inclusive
     std::uniform_int_distribution<int> prio(1, 4);
 
+    //Initialize variables
     process.arrivalTime = arrival(gen);
     process.expectedRunTime = runtime(gen);
     process.priority = prio(gen);
@@ -33,15 +39,25 @@ Process createProcess(){
     return process;
 }
 
+//Logic for sorting processes.  If true, proc1 goes before proc2
 bool arrivaltimeSort(const Process& proc1, const Process& proc2){
     return proc1.arrivalTime < proc2.arrivalTime;
 }
 
+//Function to return a queue of processes sorted by arrival time
 std::queue<Process> createProcessQueue(int numProcesses){
 
-    std::vector<Process> procs;
-    int iterator = 0;
+    //Input checking
+    if(numProcesses <= 0 ){
+        std::cerr << "\033[31m" <<"ERROR: INVALID NUMBER OF PROCESSES.  MUST ENTER A POSITIVE NUMBER." << "\033[0m" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
+    //Create a Vector which can be sorted with std::sort
+    std::vector<Process> procs;
+
+    //Logic to assign alphabetical id's to each process
+    int iterator = 0;
     for(char id = 'A'; id < 'Z'; id++){
         Process proc = createProcess();
         proc.id = id;
@@ -55,10 +71,13 @@ std::queue<Process> createProcessQueue(int numProcesses){
         }     
     }
 
+    //Sort the Vector in-place by arrival time.
     std::sort(procs.begin(), procs.end(), &arrivaltimeSort);
 
+    //Create output queue
     std::queue<Process> procsout;
     
+    //Copy the Vector into the queue
     for(int i = 0; i < numProcesses; i++){
         procsout.push(procs[i]);
     }

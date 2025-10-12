c6daee8483140d6ba42efa2ece6e1c9550327afa
KingMongkut <ethan.d.shenassa@gmail.com>
2025-10-12 13:27:02 -0700

    Updated srt

--- Project 2 - CPU Scheduling/src/SRT.h
+++ Project 2 - CPU Scheduling/src/SRT.h
@@ -8,33 +8,33 @@ Ethan
 #include "Processes.h"
 
 
-struct Job {
-	float arrivalTime;
-	float completionTime;
-	float expectedRunTime;
-	float initialExpectedRunTime;	// this is just to confirm wait time
-    int priority;
-    char id;
+// struct Job {
+// 	float arrivalTime;
+// 	float completionTime;
+// 	float expectedRunTime;
+// 	float initialExpectedRunTime;	// this is just to confirm wait time
+//     int priority;
+//     char id;
 
-	float startTime;
-	float turnaroundTime;
-	float responseTime;
-	float waitTime;
+// 	float startTime;
+// 	float turnaroundTime;
+// 	float responseTime;
+// 	float waitTime;
 
-    // constructor for when job enters ready queue
-    Job(float arrivalTime, int priority, char id, float expectedRunTime, float startTime = -1);
-    Job(Process proc);
+//     // constructor for when job enters ready queue
+//     Job(float arrivalTime, int priority, char id, float expectedRunTime, float startTime = -1);
+//     Job(Process proc);
     
-    // sets the start time of process (happens once)
-	float setStartTime(float time) ;
-    // {   return this.startTime = (this.startTime < 0 ? time : this.startTime);   }
+//     // sets the start time of process (happens once)
+// 	float setStartTime(float time) ;
+//     // {   return this.startTime = (this.startTime < 0 ? time : this.startTime);   }
 
-    //for when job actually finishes executing, calculate turnaround time, etc.
-    void finish(float time);
-};
+//     //for when job actually finishes executing, calculate turnaround time, etc.
+//     void finish(float time);
+// };
 
 // Logic for sorting processes.  If true, proc1 goes after proc2	*double check this
 // sorted in descending order to make popping easier
-bool remainingTimeSort(const Job& job1, const Job& job2);
+bool remainingTimeSort(const Process& job1, const Process& job2);
 
 int SRT(std::queue<Process> &processes);
--- Project 2 - CPU Scheduling/src/srt.cpp
+++ Project 2 - CPU Scheduling/src/srt.cpp
@@ -4,95 +4,53 @@
 # include <iostream>
 # include "SRT.h"
 
-// constructor for when job enters ready queue
-Job::Job(float arrivalTime, int priority, char id, float expectedRunTime, float startTime) {
-	this->arrivalTime = arrivalTime;
-	this->completionTime = -1;
-	this->expectedRunTime = expectedRunTime;
-	this->initialExpectedRunTime = expectedRunTime;
-	this->priority = priority;
-	this->id = id;
-
-	this->startTime = startTime;
-	this->turnaroundTime = -1;
-	this->responseTime = -1;
-	this->waitTime = 0;
+void printq(std::vector<Process> q) {
+    for (auto &it : q) {
+        std::cout << it.id << " ";
+    }
 }
-
-Job::Job(Process proc) {
-	this->arrivalTime = proc.arrivalTime;
-	this->completionTime = -1;
-	this->expectedRunTime = proc.expectedRunTime;
-	this->initialExpectedRunTime = proc.expectedRunTime;
-	this->priority = proc.priority;
-	this->id = proc.id;
-
-	this->startTime = -1;
-	this->turnaroundTime = -1;
-	this->responseTime = -1;
-	this->waitTime = 0;
-	
-}
-
-// sets the start time of process (happens once)
-float Job::setStartTime(float time) {
-	if (this->startTime < 0) {
-		this->startTime = time;
-		return time;
-	}
-	else {
-		return -1;
-	}
-}
-
-
-void Job::finish(float time) {
-	if (this->expectedRunTime > 0)	std::cout<<"something's fishy"<<std::endl;
-	this->completionTime = time;
-	this->turnaroundTime = this->completionTime-this->arrivalTime;
-	this->responseTime = this->startTime-this->arrivalTime;
-}
-
 
 int SRT(std::queue<Process> &processes) {
-	int numproc = processes.size();
 	int quanta = 0;
-	std::vector<Job> arrivedJobs;	// ready queue
-	std::vector<Job> completedJobs;
-
-	while(!processes.empty() || !arrivedJobs.empty()) {
+	std::vector<Process> ready; // ready queue
+	std::vector<Process> finished;
+	while(!processes.empty() || !ready.empty()) {
 		// each new quantum, move any new jobs that arrived into the ready queue
-		if (processes.front().arrivalTime <= quanta) {
+		if (!processes.empty() && processes.front().arrivalTime <= quanta) {
 			while(processes.front().arrivalTime <= quanta) {
-				arrivedJobs.push_back(Job(processes.front()));
+				ready.push_back(processes.front());
 				processes.pop();
 			}
 			// sort the arrived jobs by process runtime (descending)
 			// problem: if a new job arrives with the same remaining time as the current job, it might preempt the already running one
-			std::sort(arrivedJobs.begin(), arrivedJobs.end(), &remainingTimeSort);
+			
+            std::sort(ready.begin(), ready.end(), &remainingTimeSort);
 		}
+        printq(ready);
+        printq(finished);
 
-		arrivedJobs.back().setStartTime(quanta);
-		arrivedJobs.back().expectedRunTime--;
-		for (auto &it : arrivedJobs) {	// jobs that are ready but not running are waiting
+		ready.back().setstartTime(quanta);
+		ready.back().expectedRunTime--;
+		for (auto &it : ready) {	// jobs that are ready but not running are waiting
 			it.waitTime++;
 		}
-		arrivedJobs.back().waitTime--;	// okay i know this is silly—sue me
+		ready.back().waitTime--;	// okay i know this is silly—sue me
 
-		if (arrivedJobs.back().expectedRunTime <= 0) {
-			completedJobs.push_back(arrivedJobs.back());
-			arrivedJobs.pop_back();
-			std::cout << "finished job " << completedJobs.back().id << " at time slice " << quanta << std::endl;
+		if (ready.back().expectedRunTime <= 0) {
+			finished.push_back(ready.back());
+			ready.pop_back();
+			std::cout << "finished job " << finished.back().id << " at time slice " << quanta << std::endl;
 			// now arrivalTime shows Turnaround Time for process
-			completedJobs.back().completionTime = quanta;
+			finished.back().completionTime = quanta;
 		}
 		quanta++;
 	}
 
+    return 0;
 }
 
 // Logic for sorting processes.  If true, proc1 goes after proc2	*double check this
 // sorted in descending order to make popping easier
-bool remainingTimeSort(const Job& job1, const Job& job2){
+bool remainingTimeSort(const Process& job1, const Process& job2){
     return job1.expectedRunTime > job2.expectedRunTime;
 }

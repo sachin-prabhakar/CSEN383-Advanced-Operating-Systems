e08a9e2bc239113399917d30b0968810a738caab
LiamRobertson327 <liam327@verizon.net>
2025-10-14 11:58:19 -0700

    Added some thread logic.

--- Project 3 - Memory Management/src/main.cpp
+++ Project 3 - Memory Management/src/main.cpp
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
-#include <typeinfo>
-#include <string>
+#include <string>   //std::string
+#include <array>    //std::array
+#include <queue>    //std::queue
+#include <pthread.h>
+
+#include "ticketSellers.h"  //Functions to run the threads on
 
 int main(int argc, char* argv[]){
 
@@ -27,9 +31,36 @@ int main(int argc, char* argv[]){
         std::cerr<<"Argument must be an integer"<<std::endl;
         exit(EXIT_FAILURE);
     }
-    
-    
 
+    //Create a 10x10 array to keep track of the seats
+    std::array<std::array<std::string,10>,10> seats;
+    
+    //Threads for each seller
+    pthread_t H1, M1, M2, M3, L1, L2, L3, L4, L5, L6;
+
+    //(thread, attributes for the thread, function to run, args for function)
+    pthread_create(&H1, NULL, highTickets, NULL);
+    pthread_create(&M1, NULL, MediumTickets, NULL);
+    pthread_create(&M2, NULL, MediumTickets, NULL);
+    pthread_create(&M3, NULL, MediumTickets, NULL);
+    pthread_create(&L1, NULL, LowTickets, NULL);
+    pthread_create(&L2, NULL, LowTickets, NULL);
+    pthread_create(&L3, NULL, LowTickets, NULL);
+    pthread_create(&L4, NULL, LowTickets, NULL);
+    pthread_create(&L5, NULL, LowTickets, NULL);
+    pthread_create(&L6, NULL, LowTickets, NULL);
+
+    //(thread to join, return value)
+    pthread_join(H1, NULL);
+    pthread_join(M1, NULL);
+    pthread_join(M2, NULL);
+    pthread_join(M3, NULL);
+    pthread_join(L1, NULL);
+    pthread_join(L2, NULL);
+    pthread_join(L3, NULL);
+    pthread_join(L4, NULL);
+    pthread_join(L5, NULL);
+    pthread_join(L6, NULL);
 
     return 1;
 }

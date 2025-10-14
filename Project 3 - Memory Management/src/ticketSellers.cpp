656e65ab9728b1038245f9741ee20c056edd819e
LiamRobertson327 <liam327@verizon.net>
2025-10-14 11:57:53 -0700

    Updated arguments of seller functions.

--- Project 3 - Memory Management/src/ticketSellers.cpp
+++ Project 3 - Memory Management/src/ticketSellers.cpp
@@ -1,19 +1,19 @@
 #include "ticketSellers.h"
 
 //Function for High priced ticket seller
-void *highTickets(){
+void *highTickets(void *arg){
 
 
 }
 
 //Function for Medium priced ticket seller
-void *MediumTickets(){
+void *MediumTickets(void *arg){
 
 
 }
 
 //Function for Low priced ticket seller
-void *LowTickets(){
+void *LowTickets(void *arg){
 
-    
+
 }
--- Project 3 - Memory Management/src/ticketSellers.h
+++ Project 3 - Memory Management/src/ticketSellers.h
@@ -5,10 +5,10 @@
 #include <queue>    //std::queue
 
 //Function for High priced ticket seller
-void *highTickets();
+void *highTickets(void *arg);
 
 //Function for Medium priced ticket seller
-void *MediumTickets();
+void *MediumTickets(void *arg);
 
 //Function for Low priced ticket seller
-void *LowTickets();
+void *LowTickets(void *arg);

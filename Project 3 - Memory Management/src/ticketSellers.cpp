8be4e07751809f946567aea7f58bfad074a18530
LiamRobertson327 <liam327@verizon.net>
2025-10-15 11:35:55 -0700

    Finalized skeleton of function to implement.

--- Project 3 - Memory Management/src/ticketSellers.cpp
+++ Project 3 - Memory Management/src/ticketSellers.cpp
@@ -6,69 +6,75 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 //Create a 10x10 array to keep track of the seats
 std::array<std::array<std::string,10>,10> seats;
 
-//Function for High priced ticket seller
-void *highTickets(void *arg){
-    int N = *static_cast<int*>(arg);
+//Used to synchronize the 10 threads before they begin the main loop
+std::barrier sync_threads(10);
 
-    for(int minute = 0; minute < 60; minute ++){
+//Function for ticket seller
+void *ticketSeller(void *arg){
+    int trackedTime = 0;
 
-        //If there is a customer ....
+    threadData *data = static_cast<threadData*>(arg);
 
-        pthread_mutex_lock(&mutex);
-        /*
-        Find a ticket for the customer and take, randomly,
-        1 or 2 minutes to complete a sale.
-        
-        Start from row 1 and search towards the back
-        */
-        pthread_mutex_unlock(&mutex);
+    int N = data->num_customers;
+    char t = data->seller_type;
+
+    //Generate a random seed
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    //Arrival time: Int between 0 and 60 non inclusive
+    std::uniform_int_distribution<int> arrival(0, 59);
+
+    //Distributions for threads
+    std::mt19937 seed(rd());
+    std::uniform_int_distribution<int> H_time(1, 2);
+    std::uniform_int_distribution<int> M_time(2, 4);
+    std::uniform_int_distribution<int> L_time(4, 7);
+
+
+    std::priority_queue<int, std::vector<int>, std::greater<int>> Customers;
+
+    for(int j=0; j<N; j++){
+        Customers.push(arrival(gen));
     }
-    return NULL;
-}
 
-//Function for Medium priced ticket seller
-void *MediumTickets(void *arg){
-    int N = *static_cast<int*>(arg);
+    //Wait for all threads to arrive at this point before continuing.
+    sync_threads.arrive_and_wait();
 
-    for(int minute = 0; minute < 60; minute ++){
+    //Loop condition for ticket sellers
+    while(!Customers.empty() || trackedTime < 60){
 
-        //If there is a customer ....
+        if(Customers.top() <= trackedTime){
+            pthread_mutex_lock(&mutex);
 
-        pthread_mutex_lock(&mutex);
-        /*
-        Find a ticket for the customer and take, randomly,
-        2, 3, or 4 minutes to complete a sale.
-        
-        Start from row 5 then 6 then 4 then 7 ...
-        */
-        pthread_mutex_unlock(&mutex);
+            switch(t){
+                case 'H':
+                    /*
+                    Search for an empty seat depending on which type of seller this is
+                    
+                    */
+                    trackedTime = trackedTime + H_time(seed);
+
+                case 'M':
+                    /*
+                    Search for an empty seat depending on which type of seller this is
+                    
+                    */
+                    trackedTime = trackedTime + M_time(seed);
+
+                case 'L':
+                    /*
+                    Search for an empty seat depending on which type of seller this is
+                    
+                    */
+                    trackedTime = trackedTime + L_time(seed);
+            }
+            pthread_mutex_unlock(&mutex);
+        }else{
+            trackedTime = trackedTime + 1;
+        }   
     }
-    return NULL;
-}
-
-//Function for Low priced ticket seller
-void *LowTickets(void *arg){
-    int N = *static_cast<int*>(arg);
-
-    for(int minute = 0; minute < 60; minute ++){
-
-    
-        //If there is a customer ....
-
-        pthread_mutex_lock(&mutex);
-        /*
-        Find a ticket for the customer and take, randomly,
-        4, 5, 6, or 7 minutes to complete a sale.
-        
-        Start from row 10 and search towards the front
-        */
-        pthread_mutex_unlock(&mutex);
-    }
-    return NULL;
-}
-
-//Function to add customers to ticket sellers queues
-void *moreCustomers(void *arg){
 
     return NULL;
+
 }
--- Project 3 - Memory Management/src/ticketSellers.h
+++ Project 3 - Memory Management/src/ticketSellers.h
@@ -5,6 +5,14 @@
 #include <string>   //std::string
 #include <array>
 #include <queue>    //std::queue
+#include <random>
+#include <barrier>  //syncing threads
+
+struct threadData{
+    int num_customers;
+    char seller_type;
+    threadData(int c, char t) : num_customers(c), seller_type(t){}
+};
 
 
 //Mutex used to lock the datastructure
@@ -13,14 +21,5 @@ extern pthread_mutex_t mutex;
 //Create a 10x10 array to keep track of the seats
 extern std::array<std::array<std::string,10>,10> seats;
 
-//Function for High priced ticket seller
-void *highTickets(void *arg);
-
-//Function for Medium priced ticket seller
-void *MediumTickets(void *arg);
-
-//Function for Low priced ticket seller
-void *LowTickets(void *arg);
-
-//Function to add customers to ticket sellers queues
-void *moreCustomers(void *arg);
+//Function for ticket seller
+void *ticketSeller(void *arg);

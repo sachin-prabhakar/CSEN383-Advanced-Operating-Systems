b96d73a7c3b3bb15d6517605a14bfcb781df17af
KingMongkut <ethan.d.shenassa@gmail.com>
2025-10-19 19:58:41 -0700

    Initial commit for Project 4 Created basic Process class in page.h and page.cpp Created generateJobs() function added Makefile, .gitignore, and main.cpp files

---
+++ Project 4 - File Systems/src/main.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <pthread.h>
+#include "page.h"  //Functions to run the threads on
+
+
+int main(int argc, char* argv[]){
+    if (argc > 2) {
+        std::cout<<"USAGE: ./main [seed]"<<std::endl;
+        return 1;
+    }
+    
+
+    return 0;
+}

5c5380f6730be1af06360c9df75b1cdd8da6d0d5
LiamRobertson327 <liam327@verizon.net>
2025-10-30 15:07:20 -0700

    Addes ome metrics

--- Project 4 - Paging/src/jobs.h
+++ Project 4 - Paging/src/jobs.h
@@ -13,11 +13,15 @@ struct Job {
 
     int remainingTime;  // time remaining for process
     static int numProcs; // id for each process
-
+    int currentPage;
+    
     Job() : id(0), procSize(0), serviceTime(0), arrivalTime(0), remainingTime(0) {}
     Job(int size, int service, int arrival) : id(++numProcs), procSize(size), serviceTime(service), arrivalTime(arrival), remainingTime(-1) {}
     Job(uint32_t seed);
     friend std::ostream& operator<<(std::ostream& os, const Job& job);
+
+    int run(uint32_t seed = 42);
+
 };
 
 struct JobNode {
@@ -27,6 +31,17 @@ struct JobNode {
 };
 
 class JobList {
+    private:
+        JobNode* head;
+    public:
+        JobList() : head(nullptr) {}
+
+        void addJob_Start(JobNode* newJob);
+        void addJob_End(JobNode* newJob);
+        void addJob_Sorted(JobNode* newJob);     
+};
+
+class MemList {
     private:
         JobNode* head;
     public:

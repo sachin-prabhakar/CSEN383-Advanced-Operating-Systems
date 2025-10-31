8b3dd99a6873b3c3c20454229df530cad8a1b2fa
LiamRobertson327 <liam327@verizon.net>
2025-10-31 11:17:03 -0700

    Removed redundant includes.

--- Project 4 - Paging/src/memory.h
+++ Project 4 - Paging/src/memory.h
@@ -1,30 +1,20 @@
 #pragma once
-
 #include <random>
-#include <list>
 #include <iostream>
 #include <vector>
 #include <map>
-#include <queue>
-#include <vector>
 #include "pageTable.h"
 #include "jobs.h"
 
-
-
-
 struct Memory {
-    JobList *JobsHead;    // jobs that do not have memory allocated
     std::vector<Job> jobQueue;  // jobs that have not yet had any memory allocated   
     std::vector<Job> running;   // all currently running jobs (with allocated pages); pagetable stuff here
     std::vector<Job> finished;
     std::vector<int> freeList;
-
-
     Memory(uint32_t seed, int numJobs = 150);
+
     //Core memory management functions
     int numFree();     // returns number of free pages
-    void print(){FreeListHead->print();}
-    int run();
     void print();
+    int run();
 };
